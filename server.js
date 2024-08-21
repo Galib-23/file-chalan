f292bc442f963e5a43804afdde261d31d1a9d15c
Galib-23 <aagalib2323@gmail.com>
2024-08-21 14:56:01 +0600

    connect mongoose

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const multer = require('multer');
+const dotenv = require('dotenv');
+const { connectToDb } = require('./db/connectToDb.js');
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const upload = multer({ dest: 'uploads'});
+
+connectToDb();
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.render("index");
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('hi')
 })
 
 app.listen(PORT, () => {
