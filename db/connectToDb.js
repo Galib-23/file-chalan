93e47c23e1050fdaa6ec1e85b10b9d473d4e1caf
Galib-23 <aagalib2323@gmail.com>
2024-08-21 15:10:32 +0600

    add file model

--- db/connectToDb.js
+++ db/connectToDb.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectToDb = () => {
+const connectToDb = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB!");
   } catch (error) {
-    console.log("Error connecting mongo: ", error.message);
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
