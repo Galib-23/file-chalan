a57191f4c5a1961fc7f665a3df6d52e6ebd62502
Galib-23 <aagalib2323@gmail.com>
2024-08-21 20:10:34 +0600

    create upload route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const multer = require('multer');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 const { connectToDb } = require('./db/connectToDb.js');
+const { File } = require('./models/file.model.js');
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,8 +19,18 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    res.send('hi')
+app.post('/upload', upload.single('file'), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password !== null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const file = await File.create(fileData);
+    console.log(file)
+    res.send(file.originalName)
 })
 
 app.listen(PORT, () => {
